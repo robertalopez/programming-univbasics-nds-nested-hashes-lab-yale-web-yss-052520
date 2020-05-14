7795784faeefbb33f26470c357ce83c4b1c1b4bb
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 20:54:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,6 +22,7 @@ def literal_aoh
   # held in the constants
   array = [
     {name: "Don Gately", occupation: "Live-in Staff"}
+    {name:  "Joelle van Dyne", occupation: "Radio Personality" }
     ]
 end
 
